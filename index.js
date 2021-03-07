c9c6e9b9fd3a6f329293cf7b1dc7f77202ab522d
OmerBotbol <omerbot2@gmail.com>
2021-03-07 13:55:58 +0200

    exercise 3.1

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -20,10 +23,35 @@ let notes = [
     important: true
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.post('/api/notes', (request, response) => {
+    const note = request.body;
+    console.log(note);
+
+    response.send(note);
+  })
 
 const PORT = 3001
 app.listen(PORT)
