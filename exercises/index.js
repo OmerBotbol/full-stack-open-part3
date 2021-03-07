b9ffe172fedadc471933a845de73576883b6fe0a
OmerBotbol <omerbot2@gmail.com>
2021-03-07 14:12:53 +0200

    exercise 3.2

--- exercises/index.js
+++ exercises/index.js
@@ -28,6 +28,15 @@ app.get("/api/persons",((req, res)=>{
     res.send(persons);
 }));
 
+app.get("/info", ((req, res)=>{
+    const info = {
+        entries: persons.length,
+        date: new Date()
+    };
+
+    res.send(`Phonebook has info for ${info.entries} people </br> ${info.date}`)
+}))
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
