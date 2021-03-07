f11ed5236e9f032c8bce6cd698fe8e3988ccdbc1
OmerBotbol <omerbot2@gmail.com>
2021-03-07 19:39:38 +0200

    exercise 3.7 + 3.8

--- exercises/index.js
+++ exercises/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
 
 app.use(express.json());
+morgan.token("request-body", ((req, res)=>{return JSON.stringify(req.body)}))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :request-body"));
 
 const persons = [
     {
@@ -70,7 +73,6 @@ app.post("/api/persons", ((req, res)=>{
             return createID()
         }
         else{
-            console.log("new id: " + newID)
             return newID;
         }
     }
