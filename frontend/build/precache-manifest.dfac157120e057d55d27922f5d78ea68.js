ac1172796f25fe4e7e5db488964d2eb0557020a8
OmerBotbol <omerbot2@gmail.com>
2021-03-08 13:17:30 +0200

    adding build folder

---
+++ frontend/build/precache-manifest.dfac157120e057d55d27922f5d78ea68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a078390bfa994e4983537cc5b4df1640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab48a065bb4c92fd5c19",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "1224e9f809dff3239121",
+    "url": "/static/js/2.870ca22c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.870ca22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab48a065bb4c92fd5c19",
+    "url": "/static/js/main.5ff5295a.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
