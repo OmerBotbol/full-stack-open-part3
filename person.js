bab72bc4755fd4553e3ab2cb1894f5e975060ee8
OmerBotbol <omerbot2@gmail.com>
2021-03-09 16:43:43 +0200

    exercise 3.20

--- person.js
+++ person.js
@@ -10,8 +10,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}).then((
 })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, unique: true, minlength: 8 },
 })
 personSchema.plugin(uniqueValidator);
 
